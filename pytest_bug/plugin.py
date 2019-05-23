56c01919c7b4c1474ef45296d7f3e7c0cd98e040
tolstislon <tolstislon@gmail.com>
2019-05-23 20:59:58 +0300

    support 3.4, 3.5

--- pytest_bug/plugin.py
+++ pytest_bug/plugin.py
@@ -1,5 +1,4 @@
 import enum
-from typing import Tuple
 
 import pytest
 
@@ -15,7 +14,7 @@ class MARKS(enum.Enum):
     UNKNOWN = 'u'
 
 
-def bug_mark(*args, run: bool = False, **kwargs) -> Tuple[str, bool]:
+def bug_mark(*args, run=False, **kwargs):
     com = [str(i) for i in args]
     com.extend(str(i) for i in kwargs.values())
     return ', '.join(com), run
@@ -76,5 +75,6 @@ class PyTestBug:
 
     def pytest_terminal_summary(self, terminalreporter):
         terminalreporter.write_sep(
-            '-', f'Bugs skipped: {self._skipped}, Bugs passed: {self._passed}, Bugs failed: {self._failed}'
+            '-',
+            'Bugs skipped: {0}, Bugs passed: {1}, Bugs failed: {2}'.format(self._skipped, self._passed, self._failed)
         )
