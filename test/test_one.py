f16849ceda7a9fb2da660cd1d4e11637e145abf5
tolstislon <tolstislon@gmail.com>
2019-05-23 19:26:37 +0300

    add tests

--- test/test_one.py
+++ test/test_one.py
@@ -14,3 +14,23 @@ def test_two():
 @pytest.mark.bug('pass', run=True)
 def test_three():
     assert True
+
+
+@pytest.mark.bug('skip class')
+class TestFour:
+
+    def test_one(self):
+        assert False
+
+    def test_two(self):
+        assert True
+
+
+@pytest.mark.bug('class', run=True)
+class TestFive:
+
+    def test_one(self):
+        assert False
+
+    def test_two(self):
+        assert True
--- test/test_two.py
+++ test/test_two.py
@@ -14,3 +14,23 @@ def test_two():
 @bug('pass', run=True)
 def test_three():
     assert True
+
+
+@bug('skip class')
+class TestFour:
+
+    def test_one(self):
+        assert False
+
+    def test_two(self):
+        assert True
+
+
+@bug('class', run=True)
+class TestFive:
+
+    def test_one(self):
+        assert False
+
+    def test_two(self):
+        assert True
