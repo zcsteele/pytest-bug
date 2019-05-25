31ef4620ebcc0bb92dc2f0dabaf528291dff088a
tolstislon <tolstislon@gmail.com>
2019-05-25 15:04:38 +0300

    set comments

--- pytest_bug/plugin.py
+++ pytest_bug/plugin.py
@@ -2,9 +2,10 @@ import enum
 
 import pytest
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 BUG = '_mark_bug'
+COMMENT = '_bug_comment'
 
 
 class MARKS(enum.Enum):
@@ -16,8 +17,8 @@ class MARKS(enum.Enum):
 
 def bug_mark(*args, run=False, **kwargs):
     com = [str(i) for i in args]
-    com.extend(str(i) for i in kwargs.values())
-    return ', '.join(com), run
+    com.extend('%s=%s' % (key, value) for key, value in kwargs.items())
+    return ', '.join(com) if com else 'no comment', run
 
 
 class PyTestBug:
@@ -40,32 +41,38 @@ class PyTestBug:
     def set_mark(obj, mark=MARKS.UNKNOWN):
         setattr(obj, BUG, mark)
 
+    @staticmethod
+    def set_comment(obj, comment):
+        setattr(obj, COMMENT, 'BUG: %s' % str(comment))
+
     def pytest_runtest_setup(self, item):
         for i in item.iter_markers(name='bug'):
             comment, run = bug_mark(*i.args, **i.kwargs)
-            if not run:
+            self.set_comment(item, comment)
+            if run:
+                self.set_mark(item)
+            else:
                 self.set_mark(item, MARKS.SKIP)
                 pytest.skip(comment)
-            else:
-                self.set_mark(item)
 
     @pytest.hookimpl(tryfirst=True, hookwrapper=True)
     def pytest_runtest_makereport(self, item):
         outcome = yield
-        rep = outcome.get_result()
+        report = outcome.get_result()
         mark = getattr(item, BUG, None)
         if mark:
-            if rep.skipped:
-                self.set_mark(rep, MARKS.SKIP)
-            if rep.when == 'call':
-                if rep.passed:
-                    self.set_mark(rep, MARKS.PASS)
-                elif rep.failed:
-                    rep.outcome = "skipped"
+            self.set_comment(report, getattr(item, COMMENT))
+            if report.skipped:
+                self.set_mark(report, MARKS.SKIP)
+            if report.when == 'call':
+                if report.passed:
+                    self.set_mark(report, MARKS.PASS)
+                elif report.failed:
+                    report.outcome = "skipped"
                     eval_fail = getattr(item, "_evalxfail", None)
                     if eval_fail is not None:
-                        rep.wasxfail = eval_fail.getexplanation()
-                        self.set_mark(rep, MARKS.FAIL)
+                        report.wasxfail = eval_fail.getexplanation()
+                        self.set_mark(report, MARKS.FAIL)
 
     def pytest_report_teststatus(self, report):
         mark = getattr(report, BUG, None)
